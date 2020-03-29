fe8840afa73314e16b2012faacdcdf70692d1963
bergony <bergony@hotmail.com>
2020-03-29 07:56:13 -0300

    New card, Email Send,

--- HipnoseSite/routes/users.js
+++ HipnoseSite/routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const nodemailer = require('nodemailer');
 
+
+/* GET users listing. */
+router.post('/', function(req, res) {
+
+    const body = req.body;
+    console.log(body);
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: "smtp.gmail.com",
+        auth: {
+            user: 'esphipclinica@gmail.com',
+            pass: 'bbbgatos100'
+        }
+    })
+
+
+    var mailOptions = {
+
+        from: req.body.email,
+        to: "izadoramar@gmail.com",
+        subject: req.body.cidade,
+        text: "Eu " + req.body.nome + " Melo " + req.body.Sobrenome + " moro na cidade de " + req.body.cidade + "/" + req.body.estado + ", " + req.body.endereco + " no cep " + req.body.cep + ", meu telefone para contato " + req.body.telefone + " e o meu email " + req.body.email + ". Gostaria de agendar uma avaliação."
+    };
+
+    console.log(mailOptions);
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    return res.redirect("/");
+});
 module.exports = router;
